d97c30cdce21cbd6823b96e701743346b0bbefb7
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-04-10 12:26:00 +0800

    initial commit: generate project

---
+++ src/main/java/info/startupbuilder/stocks/pse/data/extractor/PseDataExtractorApplication.java
@@ -0,0 +1,13 @@
+package info.startupbuilder.stocks.pse.data.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseDataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseDataExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/startupbuilder/stocks/pse/data/extractor/PseDataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package info.startupbuilder.stocks.pse.data.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseDataExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
